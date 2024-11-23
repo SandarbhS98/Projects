dc00069616525470e448c7e65f6729f4cdd93b7e
SandarbhSahu <sssandarbhsahu@gmail.com>
2024-11-23 23:35:26 +0530

    RestApis Revision

---
+++ RestAPIRevis/src/test/java/com/learn/Rest/RestApiRevisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.Rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiRevisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
